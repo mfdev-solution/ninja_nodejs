7dfe89ba1a5348ad0742e8066d9723828368bfc8
mfdev-solution <lasolution.mfdev@gmail.com>
2022-04-17 04:44:10 +0000

    writing files and creating directories

--- file.js
+++ file.js
@@ -5,5 +5,19 @@ fs.readFile('./docs/hello.txt',(err,data) =>{
     if(err){
         console.log(err);
     }
-    console.log(data.toString());
+     console.log(data.toString());
 })
+
+//writing files 
+fs.writeFile('./docs/blog1.txt' , "hello world",()=>{
+    console.log('file was writen')
+})
+
+if(!fs.existsSync('./assets')){
+    fs.mkdir('./assets',(err) =>{
+        if(err){
+            console.log(err);
+        }
+        console.log('folder created')
+    })
+}
