dc81089aafb0097357d2b4ea439ffa6a97990d81
mfdev-solution <lasolution.mfdev@gmail.com>
2022-04-18 03:11:01 +0000

    installing nodemon

--- app.js
+++ app.js
@@ -7,21 +7,24 @@ const app = express();
 
 //listen for requests
 //setting views 
-app.set('views engine', 'ejs')
+app.set('view engine', 'ejs')
 
 
 app.listen(3000);
 
 app.get('/', (req,res) => {
     // res.send("<p>home page </p>");
-    res.sendFile('./views/index.html',{root:__dirname})
+    res.render('index');
+    // res.sendFile('./views/index.html',{root:__dirname})
 })
 
 
 app.get('/about', (req,res) => { 
+    res.render('index');
     // res.send("<p>home page </p>");
-    res.sendFile('./views/about.html',{root:__dirname})
+    // res.sendFile('./views/about.html',{root:__dirname})
 })
 app.use((req,res) =>{
-    res.sendFile('./views/404.html',{root:__dirname})
+    res.render('index');
+    // res.sendFile('./views/404.html',{root:__dirname})
 })
