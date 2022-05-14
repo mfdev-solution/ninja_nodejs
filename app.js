bdc03952d1cc502ce8856426ea69e7260c8e061d
mfdev-solution <lasolution.mfdev@gmail.com>
2022-05-14 01:22:16 +0000

    lesson 11

--- app.js
+++ app.js
@@ -1,93 +1,43 @@
 const express = require('express');
-const { render } = require('express/lib/response');
-const mongoose = require('mongoose')
-const Blog = require('./models/blogs');
-// express app
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
+// express app
 const app = express();
 
-//listen for requests
-//setting views 
-app.set('view engine', 'ejs')
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
 
-//connect to mongodb
-const URIDB = 'mongodb+srv://mfdev:fall1999m@cluster0.dtefj.mongodb.net/note-tuts?retryWrites=true&w=majority'
-// const URIDB = 'mongodb+srv://mfdev:<password>@mycluster.hv2m1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-mongoose.connect('mongodb://localhost/my_database')
-.then((result) =>  app.listen(3000))
-.catch((err)=>console.log(err));
-// static files 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
-//mongoose and sandbox routes
-app.get('/add-blog' ,(req, res)=>{
-    const blog = new Blog({
-        title:'new Blog',
-        snippet:"about my new blog",
-        body:"more about my new blog"
-    })
-    blog.save()
-});
-app.get('/blogs',(req,res)=>{
-    Blog.find()
-        .then((resutl)=>{
-            res.render('blogs',{title:'Blogs',blogs:resutl})
-            })
-    // const Blogs = Blog.find({})
-    // console.log(Blogs);
-    // res.render('index',{title:'blogs',blogs:Blogs})
-});
-app.get('/single-blog',(req,res)=>{
-    Blog.findById('6260e7ed78b4e7d84cf564d5')
-    .then((result)=>{
-        res.render('blog',{title:result.title,blog:result});
-    })
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
 
-    
-    app.get('/', (req, res) => {
-        const blogs = [
-            {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-            {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-            {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        ];
-        res.render('index', { title: 'Home', blogs });
-    }
-    );
-    
-    
-    app.get('/about', (req,res) => { 
-        res.render('about',{title:"about"});
-        // res.send("<p>home page </p>");
-        // res.sendFile('./views/about.html',{root:__dirname})
-    })
-    app.get('/blogs/create',(req,res) =>{
-        res.render('create',{title:"create a new blog"});
-    } );
-    app.post('/blogs',(req,res)=>{ 
-        const blogs = new Blog(req.body);
-        blogs.save()
-        .then((result) =>{
-            res.redirect('/blogs');
-        })
-        
-    })
-    app.get('/blogs/:id',(req,res) =>{
-        const id = req.params.id;
-        Blog.findById(id)
-        .then((result)=>{
-            res.render('blog',{title:result.title,blog:result});
-        })
-    });
-app.delete('/blogs/:id', (req , res ) =>{
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-    .then(result=>{
-        res.json({redirect:'/blogs'});
-    })
-} )
-app.use((req,res) =>{
-    res.status(404).render('404',{title:"404"});
-    // res.sendFile('./views/404.html',{root:__dirname})
-})
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
+app.use(express.urlencoded({extended:true}))
+
 app.set('view engine', 'ejs')
 app.listen(12345)
 app.use((req, res)=>{
+
+        console.log("===============headers================= \n")
         console.log(req.headers);
         console.log(req.protocol);
         console.log(req.body);
         console.log(req.query);
+        // console.log(res.)
+        // console.log(req.baseUrl);
 })
 
